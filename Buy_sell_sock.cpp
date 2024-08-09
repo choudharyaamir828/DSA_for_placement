e34b2b4415e9a472942560dbaa264b520a953cf4
mohd Amir <choudharyaamir828@gmail.com>
2024-08-09 10:15:15 +0530

    add itrative function to

--- Buy_sell_sock.cpp
+++ Buy_sell_sock.cpp
@@ -1,28 +1,42 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-void  BuySell_stock(vector<int>&arr,int i,int &max_profit,int &min_price){
+void  BuySell_stock(vector<int>&arr,int &max_profit,int &min_price){
 
     
-    
 
-    
-    if(i == arr.size()){
-        return ;
-    }
+    for(int i =0;i<=arr.size();i++){
 
-    // ek case
-    if(arr[i] < min_price){
-        min_price = arr[i];
-    }
-    int todayprofit = arr[i] - min_price;
-    if(todayprofit > max_profit ){
-        max_profit = todayprofit;
-    }
+        if(arr[i] < min_price){
+            min_price = arr[i];
+        }
+        int todayprofit = arr[i] - min_price;
+        if(todayprofit > max_profit ){
+            max_profit = todayprofit;
+        }
 
-    // recursion call 
-    BuySell_stock(arr,i+1,max_profit,min_price);
+    }
 }
+
+
+// void  BuySell_stock(vector<int>&arr,int i,int &max_profit,int &min_price){
+
+//     if(i == arr.size()){
+//         return ;
+//     }
+
+//     // ek case
+//     if(arr[i] < min_price){
+//         min_price = arr[i];
+//     }
+//     int todayprofit = arr[i] - min_price;
+//     if(todayprofit > max_profit ){
+//         max_profit = todayprofit;
+//     }
+
+//     // recursion call 
+//     BuySell_stock(arr,i+1,max_profit,min_price);
+// }
 
 int main(){
 
@@ -31,8 +45,11 @@ int max_profit = INT_MIN;
  int min_price = INT_MAX;
 
 
- BuySell_stock(arr,0,max_profit,min_price);
-  cout<< "maximum profit is : " <<max_profit;
+//  BuySell_stock(arr,0,max_profit,min_price);(using recursion)
+
+// itrativly 
+BuySell_stock(arr,max_profit,min_price);
+cout<< "maximum profit is : " <<max_profit;
 
 
     return 0;
