4896b1dd5b4b9de7a50341fad6a6126dbfa0feb8
mohd Amir <choudharyaamir828@gmail.com>
2024-09-11 00:50:10 +0530

    friend keyword

---
+++ Basic/OOPS/friendKeyword.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+class mobile{
+    string name;
+    int price;
+
+    public:
+    mobile(string _Name,int _price):name(_Name),price(_price){};
+
+
+};
+
+class B{
+    public:
+    void print(const mobile &a){
+        
+    }
+};
+
+
+
+
+int main(){
+
+
+    return 0;
+}
