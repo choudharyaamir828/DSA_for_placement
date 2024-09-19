29e51316a3592958f2d903f135cc83b74f4f320a
mohd Amir <choudharyaamir828@gmail.com>
2024-09-19 22:54:28 +0530

    Linked list file updated

--- Basic/OOPS/LL-class-1.cpp
+++ Basic/OOPS/LL-class-1.cpp
@@ -6,42 +6,159 @@ class node{
     public:
     int data;
     node *next;
-
      node(){
         this->data = 0;
         this->next = NULL;
-
     }
     node(int data){
         this->data = data;
         this->next =  NULL;
     }
-
 };
+
 void print(node* &head){
-node* temp = head;
-while(temp != NULL){
-    cout<<temp->data<<" ";
-    temp = temp->next;
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    
 }
 
+// i want to insert a node right at the head of linked list
+
+void insertAthead(node* &head,node* &tail, int data){
+
+    if(head == NULL){
+        node* newnode = new node(data);
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    // step-1
+    node* newnode = new node(data);
+
+
+    // step-2
+    newnode ->next = head ;
+ 
+    //step-3
+
+    head = newnode;
+
 }
+
+void insertAttail(node* &head, node* &tail,int data){
+
+     if(head == NULL){
+        node* newnode = new node(data);
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    // step-1
+    node* newnode = new node(data);
+
+     // step-2
+     tail->next = newnode;
+   
+    // step-3
+    tail= newnode;
+}
+
+int findlength(node* &head){
+    int len =0;
+    node* temp = head;
+    while(temp != NULL){
+        temp = temp->next;
+        len++;
+    }
+    return len;
+}
+
+// insert at position 
+
+void insertAtposition(int position,node* &head,node* &tail,int data){
+
+    //if position 0
+    if(position == 0){
+        insertAthead(head,tail,data);
+        return;
+    }
+
+    // if positon is end;
+    int len = findlength(head);
+
+    if(position >= len){
+        insertAttail(head,tail,data);
+        return;
+    }
+
+    // at pos
+    int i = 1;
+    node* prev = head;
+    while(i < position){
+        prev = prev->next;
+        i++;
+    }
+    node* curr = prev->next;
+
+    // inserting
+    node* newnode = new node(data);
+    newnode->next = curr;
+    prev->next = newnode;
+}
+
+
 
 int main(){
    
+    
+    // node* first = new node(3);
+    // node* second = new node(4);
+    // node* third = new node(5);
+    // node* forth = new node(6);
+    // node* fifth = new node(7);
+    // node* sixth = new node(8);
+    // node* seven = new node(9);
 
-    node* first = new node(3);
-    node* second = new node(4);
-    node* third = new node(5);
-    node* forth = new node(6);
-    node* fifth = new node(7);
-    node* sixth = new node(8);
-    node* seven = new node(9);
+    
+    // first->next = second;
+    // second->next = third;
+    // third->next = forth;
+    // forth->next = fifth;
+    // fifth->next = sixth;
+    // sixth->next = seven;
 
-    print(first);
-   
+    node* head = NULL;
+    node* tail = NULL;
+
+    insertAthead(head,tail,12);
+    insertAthead(head,tail,16);
+    insertAthead(head,tail,60);
+  
+
+    insertAttail(head,tail,34);
+    insertAttail(head,tail,32);
+      
+    cout<<"printing element: ";
+    print(head);
+
+
+    insertAtposition(5,head,tail,101);
+
+
     
 
+
+    
+
+    cout<<endl;
+    cout<<"printing element: ";
+    print(head);
 
     return 0;
 }
