30b2c82292bca4fdcc4875c689ff2a578ae8b602
mohd Amir <choudharyaamir828@gmail.com>
2024-10-02 23:32:59 +0530

    removedublicate function added

--- RemoveDublicate-unsorted-LL.cpp
+++ RemoveDublicate-unsorted-LL.cpp
@@ -33,11 +33,28 @@ void print(node* head){
         temp = temp->next;
     }
 }
-void RemoveDublicate(node* head){
 
-  
+void RemoveDuplicate(node* head) {
+    node* temp1 = head;
 
+    while (temp1 != NULL) {
+        node* temp2 = temp1;
+        
+        // Check for duplicates ahead
+        while (temp2->next != NULL) {
+            if (temp1->data == temp2->next->data) {
+                // Found a duplicate
+                node* duplicateNode = temp2->next;
+                temp2->next = temp2->next->next; // Remove duplicate
+                delete duplicateNode; // Free memory for the duplicate
+            } else {
+                temp2 = temp2->next; // Move to the next node
+            }
+        }
+        temp1 = temp1->next; // Move to the next unique node
+    }
 }
+
 int main(){
     node* head = NULL;
     node* tail = NULL;
@@ -48,6 +65,9 @@ int main(){
     insertAtTail(head,tail,4);
     insertAtTail(head,tail,3);
     insertAtTail(head,tail,2);
+    print(head);
+    cout<<endl;
+    RemoveDuplicate(head);
     print(head);
 
 
